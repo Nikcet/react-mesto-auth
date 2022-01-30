18022245b458362817af98b9bfe17f4d405be005
Nikcet <mstislav.kr@yandex.ru>
2022-01-30 19:19:28 +0300

    Кнопка входа в акк в шапке

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ function Header() {
       <a href="./index.html" className="header__logo-link">
         <img src={logo} alt="#" className="header__logo" />
       </a>
+      <a href="/sign-in" className="header__login-link">Войти</a>
     </header>
   );
 }
